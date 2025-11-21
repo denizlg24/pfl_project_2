5abe7c987cb78f5370d84f68edb7822937b1689c
denizlg24 <denizlg24@gmail.com>
2025-11-21 10:37:38 +0000

    initial-commit

---
+++ family.pl
@@ -0,0 +1,17 @@
+% Simple family facts
+parent(alice, bob).
+parent(alice, carol).
+parent(bob, david).
+parent(carol, emma).
+female(alice).
+female(carol).
+female(emma).
+male(bob).
+male(david).
+
+
+lives_in(lion, savannah).
+lives_in(penguin, antarctica).
+lives_in(elephant, savannah).
+lives_in(panda, forest).
+lives_in(snake, forest).
